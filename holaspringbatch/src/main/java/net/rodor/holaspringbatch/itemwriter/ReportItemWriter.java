5848fb5e84ea6de17b3b56bb6d5910891a1c21cd
52535465S <52535465S@DES-62396.trabajo.dom>
2019-03-20 12:35:01 +0100

    se añade scheduler

--- holaspringbatch/src/main/java/net/rodor/holaspringbatch/itemwriter/ReportItemWriter.java
+++ holaspringbatch/src/main/java/net/rodor/holaspringbatch/itemwriter/ReportItemWriter.java
@@ -11,9 +11,9 @@ public class ReportItemWriter implements ItemWriter<Report> {
 	@Override
 	public void write(List<? extends Report> items) throws Exception {
 		System.out.println("Excribiendo numero de elementos...." + items.size());
-		for (Report item : items) {
+		/*for (Report item : items) {
 			System.out.println(item.toString());
-		}
+		}*/
 
 	}
 
--- holaspringbatch/src/main/java/net/rodor/holaspringbatch/main/HelloBatch.java
+++ holaspringbatch/src/main/java/net/rodor/holaspringbatch/main/HelloBatch.java
@@ -1,9 +1,5 @@
 package net.rodor.holaspringbatch.main;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,18 +11,8 @@ public class HelloBatch {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/job-hello-batch.xml");
 			
-		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-		Job job = (Job) context.getBean("helloWorldJob");
-
-		try {
-
-			JobExecution execution = jobLauncher.run(job, new JobParameters());
-			System.out.println("Exit Status : " + execution.getStatus());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("Done!");
+		
+		System.out.println("Scheduler lanzado!");
 	}
 
 }
